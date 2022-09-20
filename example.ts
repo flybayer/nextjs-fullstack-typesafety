08843c2c90de57b5aa207baf528ef5a2afce54cf
Brandon Bayer <b@bayer.ws>
2022-09-20 12:12:40 +0200

    more assert

--- example.ts
+++ example.ts
@@ -1,8 +1,15 @@
+export const thing: string | undefined = "stuff";
+
+if (!thing) {
+  throw new Error("It broke");
+}
+
+// ⬇️
+
+assert(thing, "It broke");
+
+thing;
 
 export function assert(condition: any, message: string): asserts condition {
   if (!condition) throw new Error(message);
 }
-
-
-
-.
